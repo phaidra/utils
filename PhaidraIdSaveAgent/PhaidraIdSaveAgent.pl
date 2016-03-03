1baa1df395f9651af153ef9c6d761063abe70a70
RastislavHudak <hudak.rastislav@gmail.com>
2016-03-03 16:01:23 +0100

    ids

--- PhaidraIdSaveAgent/PhaidraIdSaveAgent.pl
+++ PhaidraIdSaveAgent/PhaidraIdSaveAgent.pl
@@ -2,9 +2,10 @@
 
 =pod
 
-=head1 PhaidraHandleSaveAgent.pl (-since sinceepoch)
+=head1 PhaidraIdSaveAgent.pl (-since sinceepoch) (-irma-map-id mongooid)
 
-  -since sinceepoch - check irma database for handle records since this date
+  -since sinceepoch - check irma database for id records since this date
+  -irma-map-id mongooid - process the irma record with this mongo-object-id
 
 =cut
 
@@ -18,13 +19,11 @@ use Mojo::JSON qw(encode_json decode_json);
 use Mojo::UserAgent;
 use Mojo::URL;
 
-my $configpath = 'PhaidraHandleSaveAgent.json';
-
+my $configpath = 'PhaidraIdSaveAgent.json';
 unless(-f $configpath){
 	print "[".scalar localtime."] ", "Error: config path $configpath is not a file (or file does not exist). Usually $configpath would be a link to /etc/phaidra/...\n";
 	system ("perldoc '$0'"); exit (0);
 }
-
 unless(-r $configpath){
 	print "[".scalar localtime."] ", "Error: cannot access config: $configpath\n";
 	system ("perldoc '$0'"); exit (0);
@@ -35,8 +34,6 @@ my $config = decode_json($bytes);
 
 my $since;
 my $irma_map_id;
-my $ingest_instance;
-
 while (defined (my $arg= shift (@ARGV)))
 {
   if ($arg =~ /^-/)
@@ -62,25 +59,28 @@ while( my( $instance, $value ) = each %{$config->{phaidra_instances}} ){
 	}
 }
 
+# select the records
 my @records;
 if(defined($irma_map_id)){
 	print "[".scalar localtime."] ", "processing IRMA record id $irma_map_id\n"; 
 	my $rec = $irma_mongo->get_collection('irma.map')->find_one({'_id' => MongoDB::OID->new(value => $irma_map_id)});
 	push @records, $rec if defined $rec;
 }elsif(defined($since)){
-	print "[".scalar localtime."] ", "processing IRMA records since $since [".strftime("%m/%d/%Y %H:%M:%S",localtime($since))."]"; 
+	print "[".scalar localtime."] ", "processing IRMA records since $since [".strftime("%m/%d/%Y %H:%M:%S",localtime($since))."]\n"; 
 	my $recs = $irma_mongo->get_collection('irma.map')->find({'_created' => {'$gt' => $since } } );
 	while (my $rec = $recs->next) {
 		push @records, $rec if defined $rec;
 	}	
 }
 
+# process the records
 my $rec_cnt = scalar @records;
 print "[".scalar localtime."] ", "found $rec_cnt records\n"; 
 my $i = 0;
 for my $r (@records){
 	$i++;
 
+	# check if this id has a phaidra URL
 	unless ($r->{url} =~ /^http(s)?:\/\/([\w\.\-]+)\/(o:\d+)$/g){
 		print "[".scalar localtime."] ", "processing [$i/$rec_cnt] url[".$r->{url}."] not a phaidra url, skipping\n"; 
 		next;
@@ -91,28 +91,46 @@ for my $r (@records){
 
 	print "[".scalar localtime."] ", "processing [$i/$rec_cnt] pid[$pid] hdl[".$r->{hdl}."] instance[$instance]\n"; 
 
-	my $has_handle = has_handle($config, $instance, $pid, $r->{hdl});
-	if($has_handle > -1){
-		if($has_handle){
-			print "[".scalar localtime."] ", "hdl already saved, skipping\n"; 
-		}else{
-			if(save_handle($config, $instance, $pid, $r->{hdl})){
-				emit_event($paf_dbs{$instance}, $pid, $r->{hdl}, $r->{url});	
-			}
-		}
-	}else{
-		print "[".scalar localtime."] ", "error getting object ids, skipping\n"; 
+	# only pick up id we support
+	my @ids;
+	if(exists($r->{hdl}) && $r->{hdl} ne ''){
+		push @ids, {id => $r->{hdl}, type => 'hdl'};
 	}
+	if(exists($r->{urn}) && $r->{urn} ne ''){
+		push @ids, {id => $r->{urn}, type => 'urn'};
+	}
+	unless(scalar @ids  > 0){
+		print "[".scalar localtime."] ", "no known id found in the record, skipping\n"; 
+		next;
+	}
+
+	for my $id (@ids){
+		# check if id was saved
+		my $has_id = has_id($config, $instance, $pid, $id);
+		if(defined($has_id)){
+			if($has_id ne ''){
+				print "[".scalar localtime."] ", "$has_id already saved, skipping\n"; 
+			}else{
+				# save if it wasn't yet saved
+				if(save_id($config, $instance, $pid, $id)){			
+					# emit event for PAF	
+					emit_event($paf_dbs{$instance}, $pid, $id, $r->{url}) if exists $paf_dbs{$instance};
+				}
+			}
+		}else{
+			print "[".scalar localtime."] ", "error getting object ids, skipping\n"; 
+		}
+	}	
 	
 }
 
 exit(1);
 
-sub has_handle {
+sub has_id {
 	my $config = shift;
 	my $instance = shift;
 	my $pid = shift;
-	my $hdl = shift;
+	my $id = shift;
 
 	my $action = "/object/$pid/id";
 	my $url = Mojo::URL->new;
@@ -126,14 +144,21 @@ sub has_handle {
 	}
 
 	my $ua = Mojo::UserAgent->new;
-	my $tx = $ua->get($url);
-  	if (my $res = $tx->success) {
-  		for my $id (@{$res->{ids}}){
-  			if("hdl:$hdl" eq $id){  				
-  				return 1;
+	my $tx = $ua->get($url);	
+  	if (my $res = $tx->success) {  		
+  		for my $oid (@{$res->json->{ids}}){
+  			if($id->{type} eq 'hdl'){
+  				if("hdl:".$id->{id} eq $oid){  				
+  					return $oid;
+  				}
+  			}elsif($id->{type} eq 'urn'){
+  				if($id->{id} eq $oid){  				
+  					return $oid;
+  				}
   			}
+  			
   		}
-    	return 0;
+    	return '';
 	}else{
 		print "[".scalar localtime."] ", "ERROR searching for ids pid[$pid]:\n"; 
 		if($tx->res->json){
@@ -142,17 +167,17 @@ sub has_handle {
 			}
 		}
 		print Dumper($tx->error)."\n"; 		
-		return -1;
+		return undef;
 	}
 }
 
-sub save_handle {
+sub save_id {
 	my $config = shift;
 	my $instance = shift;
 	my $pid = shift;
-	my $hdl = shift;
+	my $id = shift;
 
-	my $action = "/object/$pid/id/hdl/add?id=$hdl";
+	my $action = "/object/$pid/id/add?".$id->{type}."=".$id->{id};
 	my $url = Mojo::URL->new;
 	$url->scheme('https');
 	$url->userinfo($config->{phaidra_instances}->{$instance}->{intcallusername}.":".$config->{phaidra_instances}->{$instance}->{intcallpassword});
@@ -170,7 +195,7 @@ sub save_handle {
   		print "[".scalar localtime."] ", "success\n"; 
     	return 1;
 	}else{
-		print "[".scalar localtime."] ", "ERROR adding handle hdl[$hdl] pid[$pid]:\n"; 
+		print "[".scalar localtime."] ", "ERROR adding id ".$id->{type}."[".$id->{id}."] pid[$pid]:\n"; 
 		if($tx->res->json){
 			if($tx->res->json->{alerts}){
 				print Dumper($tx->res->json->{alerts})."\n";
@@ -183,16 +208,16 @@ sub save_handle {
 sub emit_event {
 	my $paf_mongo = shift;
 	my $pid = shift;
-	my $hdl = shift;
+	my $id = shift;
 	my $url = shift;
 
 	$paf_mongo->get_collection('events')->insert({
 		ts_iso => ts_iso(), 
-		event => 'handle_save_finished',
-		agent => 'phaidra_handle_save',
+		event => 'id_save_finished',
+		agent => 'phaidra_id_save',
 		e => time, 
 		pid => $pid,
-		hdl => $hdl,
+		id => $id->{id},
 		url => $url
 	});
 }
